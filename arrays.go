5f38ced9b206e7069bdf14e1161d3eb341734571
Ashish Sharda <ashishjsharda@gmail.com>
2023-09-10 13:52:50 -0500

    example using array

--- arrays.go
+++ arrays.go
@@ -1,1 +1,16 @@
-package GoCollections
+package main
+
+import "fmt"
+
+func main() {
+	var numbers [5]int
+	numbers[0] = 1
+	numbers[1] = 2
+	numbers[2] = 3
+	numbers[3] = 4
+	numbers[4] = 5
+	fmt.Println("Array of numbers: ")
+	for index, value := range numbers {
+		fmt.Println("Index: ", index, " Value: ", value)
+	}
+}
